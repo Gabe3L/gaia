be2a738144233870e81535939c47751f84f1869a
Gabe3L <gabe3lynch@gmail.com>
2025-05-02 13:57:31 -0400

    Allowed the user to more easily customize the dataset they are training

--- language/ner/scripts/train.py
+++ language/ner/scripts/train.py
@@ -1,24 +1,39 @@
 import os
 import shutil
+from pathlib import Path
 
 import torch
 import pandas as pd
 from simpletransformers.ner import NERModel, NERArgs
 
+#############################################################
+
+TASK = "music"
+
+#############################################################
+
+class PathConfig:
+    NER_PATH = Path("language/ner")
+    TRAIN_DATASET = Path(NER_PATH / "data" / TASK / "train.csv")
+    VAL_DATASET = Path(NER_PATH / "data" / TASK / "val.csv")
+    BEST_MODEL_DIR = Path(NER_PATH / "weights" / TASK)
+    OUTPUT_DIR = Path(NER_PATH / "results")
+    CACHE_DIR = Path(NER_PATH / "cache")
+
 class GenerateModel:
     def __init__(self):
-        self.train_dataset = pd.read_csv("language/ner/data/music/train.csv")
-        self.val_dataset = pd.read_csv("language/ner/data/music/val.csv")
+        self.train_dataset = pd.read_csv(PathConfig.TRAIN_DATASET)
+        self.val_dataset = pd.read_csv(PathConfig.VAL_DATASET)
         self.model = self.load_model()
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
 
     def clear_cache(self):
-        if os.path.exists("language/ner/cache"):
-            shutil.rmtree("language/ner/cache")
+        if os.path.exists(PathConfig.CACHE_DIR):
+            shutil.rmtree(PathConfig.CACHE_DIR)
     
     def clear_poor_results(self):
-        if os.path.exists("language/ner/results"):
-            shutil.rmtree("language/ner/results")
+        if os.path.exists(PathConfig.OUTPUT_DIR):
+            shutil.rmtree(PathConfig.OUTPUT_DIR)
 
     def load_model(self) -> NERModel:
         model_args = NERArgs()
@@ -33,9 +48,9 @@ class GenerateModel:
         model_args.save_steps = 200
         model_args.max_seq_length = 128
         model_args.learning_rate = 3e-5
-        model_args.output_dir = "language/ner/results/"
-        model_args.best_model_dir = "language/ner/weights/"
-        model_args.cache_dir = "language/ner/cache"
+        model_args.output_dir = PathConfig.OUTPUT_DIR
+        model_args.best_model_dir = PathConfig.BEST_MODEL_DIR
+        model_args.cache_dir = PathConfig.CACHE_DIR
         model_args.labels_list = [
             'O', 
             'B-ARTIST', 'I-ARTIST', 
@@ -60,6 +75,8 @@ class GenerateModel:
         )
         result, model_outputs, predictions = self.model.eval_model(self.val_dataset)
         print(result)
+        
+#############################################################
 
 if __name__ == "__main__":
     model = GenerateModel()
